40655f0af3f4d686e7ce47fa238b3d96aee85dee
Rafael Mendes <mendesrafa.c@gmail.com>
2020-09-04 00:11:03 -0300

    minor css tweaks

--- assets/js/orderCheckout.js
+++ assets/js/orderCheckout.js
@@ -5,29 +5,29 @@
 function isOrderCompleted () {
     var order = document.querySelectorAll(".selected"); // get a nodeList with all selected items
     if (order.length === 3) { // if nodeList length = 3, all 3 items were selected
-       document.querySelector("button").removeAttribute("disabled"); // removes disabled attribute
+       document.querySelector(".checkout").removeAttribute("disabled"); // removes disabled attribute
+       document.querySelector(".checkout p").innerHTML = "Fechar o pedido";
        orderCheckout(); // calls order checkout
     }
  }
  
  /* Redirects order to whatsapp */
  
- function orderCheckout () {
-    var checkoutPrice = priceToPay.toFixed(2); // rounds price value
-    var selectedFood = document.querySelector(".food.selected .name").innerText;
-    var selectedDrink = document.querySelector(".drink.selected .name").innerText;
-    var selectedDessert = document.querySelector(".dessert.selected .name").innerText;
- 
-    descriptionCheckoutOrder(checkoutPrice, selectedFood, selectedDrink, selectedDessert);
-    
-    var checkOutWppMessage =
-    "Olá, gostaria de fazer o pedido:\n" +
-    "- Prato: " + selectedFood + "\n" +
-    "- Bebida: " + selectedDrink + "\n" +
-    "- Sobremesa: " + selectedDessert +" \n" +
-    "Total: R$ " + checkoutPrice;
-    
-    var wppLink = "https://wa.me/5521974393491?text=" + encodeURIComponent(checkOutWppMessage); // encodes wpp message
-    document.querySelector(".modal a").setAttribute("href", wppLink); // adds href link to button
+function orderCheckout () {
+   var checkoutPrice = priceToPay.toFixed(2); // rounds price value
+   var selectedFood = document.querySelector(".food.selected .name").innerText;
+   var selectedDrink = document.querySelector(".drink.selected .name").innerText;
+   var selectedDessert = document.querySelector(".dessert.selected .name").innerText;
+
+   descriptionCheckoutOrder(checkoutPrice, selectedFood, selectedDrink, selectedDessert);
+      
+   var checkOutWppMessage =
+      "Olá, gostaria de fazer o pedido:\n" +
+      "- Prato: " + selectedFood + "\n" +
+      "- Bebida: " + selectedDrink + "\n" +
+      "- Sobremesa: " + selectedDessert +" \n" +
+      "Total: R$ " + checkoutPrice;
+      
+   var wppLink = "https://wa.me/5521974393491?text=" + encodeURIComponent(checkOutWppMessage); // encodes wpp message
+   document.querySelector(".modal a").setAttribute("href", wppLink); // adds href link to button
  }
- 
